1b0f71fbc8c223dfec2d516cd6b4e91210020283
Florian Schmid <florian-engstingen@web.de>
2023-07-10 14:15:43 +0200

    Added Wikipedia as external service

---
+++ Demo-API/Wikipedia/WikipediaDataEnrichment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Demo_API.Data;
+namespace Demo_API.Wikipedia
+{
+	public class WikipediaDataEnrichment : IDataEnrichment
+	{
+        HttpClient client;
+
+		public WikipediaDataEnrichment(string uri)
+		{
+            client = new HttpClient();
+            client.BaseAddress = new Uri(uri);
+		}
+
+        public async Task<string> GetMoreData(string query)
+        {
+            string response = await client.GetStringAsync("?action=opensearch&namespace=0&search=" + query + "&limit=5&format=json");
+            Console.WriteLine("Response: " + response);
+            return response;
+        }
+    }
+}
+
