31b6f028aed6bf490ce208348a37c17982979170
Florian Schmid <florian-engstingen@web.de>
2023-07-10 15:49:29 +0200

    Add Test

---
+++ Demo-API.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Demo_API.Types;
+
+namespace Demo_API.Test;
+
+[TestClass]
+public class UnitTest1
+{
+
+    private const string TestString = "FunFact1";
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        DemoEntity demoEntity = new DemoEntity(TestString);
+        Assert.AreEqual(demoEntity.FunFact, TestString);
+    }
+}
